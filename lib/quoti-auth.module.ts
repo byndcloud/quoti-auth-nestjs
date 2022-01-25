d8366533611c56119f0240230f69bd396ab5de5b
Nathan Freire <nmf2@cin.ufpe.br>
2022-01-25 18:31:00 -0300

    feat: create quoti auth global module

--- lib/quoti-auth.module.ts
+++ lib/quoti-auth.module.ts
@@ -1,9 +1,26 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { SetupConfig } from 'quoti-auth';
-// TODO: Return Quoti Auth Provider
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { QuotiAuth, SetupConfig } from 'quoti-auth';
+
+@Global()
 @Module({})
 export class QuotiAuthModule {
   static register(quotiAuthSetupConfig: SetupConfig): DynamicModule {
-    return {};
+    const qtAuth = new QuotiAuth();
+    qtAuth.setup(quotiAuthSetupConfig)
+    return {
+      module: QuotiAuthModule,
+      providers: [
+        {
+          provide: QuotiAuth,
+          useValue: qtAuth,
+        },
+      ],
+      exports: [
+        {
+          provide: QuotiAuth,
+          useValue: qtAuth,
+        },
+      ],
+    };
   }
 }
