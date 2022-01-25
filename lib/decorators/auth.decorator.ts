82c46e6e57c05ba92c00fc146e8205ece49f5cc0
Nathan Freire <nmf2@cin.ufpe.br>
2022-01-25 18:31:35 -0300

    feat: add context-aware reflector to Authorization guard

--- lib/decorators/auth.decorator.ts
+++ lib/decorators/auth.decorator.ts
@@ -1,4 +1,5 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthenticationGuard } from '../guards/authentication.guard';
 import {
   AuthorizationGuard,
@@ -8,6 +9,6 @@ import {
 export function Auth(permissions: string[][] | CustomPermission) {
   return applyDecorators(
     SetMetadata('permissions', permissions),
-    UseGuards(AuthenticationGuard, AuthorizationGuard),
+    UseGuards(AuthenticationGuard, new AuthorizationGuard(new Reflector())),
   );
 }
